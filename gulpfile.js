d0aaea590d1b4d3413bf4e0a13f5b24fb054365f
Vikash2806 <vikashrajendran2806@gmail.com>
2023-01-01 15:18:51 +0530

    feat: sushi sass library folder structure add

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("sushi/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["sushi/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
