2cee8921bb1976801cf69898f5018d689694694b
Vikash2806 <vikashrajendran2806@gmail.com>
2023-01-03 15:10:38 +0530

    feat: purge css add in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("sushi/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("sushi/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
